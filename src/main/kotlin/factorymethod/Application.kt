eb6b157ec5d5cb2758b6313a6da31281c45cd5c6
leonardovee <leoveeph@gmail.com>
2022-07-13 12:28:14 -0300

    feat: remove unused val

--- src/main/kotlin/factorymethod/Application.kt
+++ src/main/kotlin/factorymethod/Application.kt
@@ -8,12 +8,11 @@ class Application {
     fun initialize() {
         val availableOs = mapOf(1 to "Windows", 2 to "Web")
 
-        val random = Random()
-        val os = availableOs.entries.elementAt(random.nextInt(availableOs.size))
+        val os = availableOs.entries.elementAt(Random().nextInt(availableOs.size))
 
         var dialog: Dialog? = null
 
-        when(os.value) {
+        when (os.value) {
             "Windows" -> dialog = WindowsDialog()
             "Web" -> dialog = WebDialog()
         }
